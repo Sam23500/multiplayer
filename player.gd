71a0abe67d5a9f926b260df628a910526192c0a0
Sam23500 <sam.scott.dot@gmail.com>
2026-05-25 19:05:54 +0100

    Initial commit

---
+++ player.gd
@@ -0,0 +1,58 @@
+extends CharacterBody3D
+
+const JUMP_VELOCITY: float = 4.5
+
+var gravity := 9.8 
+
+var max_speed: float = 5.0
+
+var syncPos := Vector3(0.0, 0.0, 0.0)
+
+func _enter_tree():
+	# Doing this here instead of on ready prevents bugs.
+	set_multiplayer_authority(int(str(name)))
+	if is_multiplayer_authority():
+		var cam = preload("res://player_cam.tscn").instantiate()
+		add_child(cam)
+
+func _ready() -> void:	
+	syncPos = global_position
+	
+	$"Player ID".text = name
+	GameManager.player_info[int(name)] = {"spawnpoint":syncPos, "node":self}
+	NetworkManager.player_disconnected.connect(_on_player_disconnected)
+	
+func _physics_process(delta: float) -> void:
+	
+	if not is_multiplayer_authority():
+		# Making it 30fps (save bandwidth) and lerping with local fps to hide the stutter
+		position = lerp(position, syncPos, 0.5)
+		return
+	
+	if not is_on_floor():
+		velocity.y -= gravity * delta
+	
+	if Input.is_action_just_pressed("jump") and is_on_floor():
+		velocity.y = JUMP_VELOCITY
+	
+	var input_dir = Input.get_vector("move_left", "move_right", "move_forward", "move_back")
+	var direction = (transform.basis * Vector3(input_dir.x, 0, input_dir.y))
+	
+	if direction:
+		direction = direction.normalized()
+		velocity.x = direction.x * max_speed
+		velocity.z = direction.z * max_speed
+	else:
+		velocity.x = move_toward(velocity.x, 0, max_speed)
+		velocity.z = move_toward(velocity.z, 0, max_speed)
+
+
+	syncPos = position
+	move_and_slide()
+
+
+func _on_player_disconnected(pid) -> void:
+	if pid == int(name):
+		GameManager.player_info.erase(pid)
+		queue_free()
+	pass
---
+++ player_cam.gd
@@ -0,0 +1,61 @@
+class_name PlayerCam
+extends Camera3D
+
+@export var mouse_sensitivity = 0.005
+
+@onready var sightline: RayCast3D = $RayCast3D
+var sight_range: float = 8
+
+var current_focus: Object
+var previous_focus: Object
+
+var active := true
+
+func _ready():
+	if is_multiplayer_authority():
+		Input.mouse_mode = Input.MOUSE_MODE_CAPTURED
+	else:
+		active = false
+	sightline.target_position = Vector3(0,0,-sight_range)
+
+func _input(event):
+	if event is InputEventMouseMotion and active:
+		get_parent().rotate_y(-event.relative.x * mouse_sensitivity)
+		
+		rotation.x -= event.relative.y * mouse_sensitivity
+		rotation.x = clamp(rotation.x, deg_to_rad(-89), deg_to_rad(89))
+
+
+func _physics_process(delta: float) -> void:
+	if Input.is_action_just_pressed("enter"):
+		if active:
+			active = false
+			Input.mouse_mode = Input.MOUSE_MODE_VISIBLE
+		else:
+			active = true
+			Input.mouse_mode = Input.MOUSE_MODE_CAPTURED
+
+	if sightline.is_colliding():
+		current_focus = sightline.get_collider()
+	else:
+		current_focus = null
+	if current_focus != previous_focus:
+			activate_focus_object(current_focus)
+			deactivate_focus_object(previous_focus)
+	previous_focus = current_focus
+
+func activate_focus_object(collider: Object):
+	if not collider is Node:
+		return
+	var target: Node = collider
+	if not target.is_in_group("Interactables"):
+		return
+	target.activate()
+
+func deactivate_focus_object(collider: Object):
+	if not collider is Node:
+		return
+	var target: Node = collider
+	if not target.is_in_group("Interactables"):
+		return
+	target.deactivate()
